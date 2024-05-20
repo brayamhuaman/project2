c7b80b480ff62ae90ebac1cd84ef044d918d41cf
Brayam <bhuamanpc@gmail.com>
2024-05-19 23:51:22 -0500

    pages  & routes

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:project2/pages/pagina1.dart';
+import 'package:project2/pages/pagina2.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Navegacion",
+      initialRoute: "pagina1",
+      routes: {"pagina1":(BuildContext context)=> const Pagina1(), 
+              "pagina2":(BuildContext context)=> const Pagina2()},
+      
+    );
+  }
+}
